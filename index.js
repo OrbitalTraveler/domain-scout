5349c894b514be26c7ecaf22972d950cd2df4e3c
NeonIT56 <jevic.it56.2022@uns.ac.rs>
2024-04-24 02:15:37 +0200

    Highway to Git Hell

---
+++ index.js
@@ -0,0 +1,12 @@
+const animatedText = document.querySelector(".animated-text");
+
+/* let index = 0;
+
+function changeText() {
+  animatedText.textContent = domains[index];
+  index = (index + 1) % domains.length;
+}
+
+changeText();
+
+setInterval(changeText, 3000); */
